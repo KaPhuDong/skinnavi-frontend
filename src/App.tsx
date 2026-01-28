452778962f5285cd6244308f0c2af75c462c3988
Hoài Tiên05 <bohonuochthihoaitien@gmail.com>
2026-01-28 10:36:17 +0700

    feat(SN-30): implement user registration

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Register from '@/features/auth/pages/Register'
+import { Toaster } from '@/shared/components/ui/toaster'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/register" replace />} />
-
         <Route path="/register" element={<Register />} />
-
-        {/* <Route path="/login" element={<LoginPage />} /> */}
-
-        {/* Route 404 - Not Found */}
         <Route
           path="*"
           element={
@@ -19,6 +15,7 @@ function App() {
           }
         />
       </Routes>
+      <Toaster />
     </BrowserRouter>
   )
 }
