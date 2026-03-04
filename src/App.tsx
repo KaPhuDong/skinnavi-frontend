b112fdfb9cc1b449515aa0a6f3389172edf31a71
Hoài Tiên05 <bohonuochthihoaitien@gmail.com>
2026-03-04 09:17:19 +0700

    feat(SN-48): build admin dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Register from '@/features/auth/pages/Register'
+import AdminDashboard from './features/admin/pages/AdminDashboard'
+import PlaceholderPage from './features/admin/components/PlaceholderPage'
 
 function App() {
   return (
@@ -8,6 +10,12 @@ function App() {
         <Route path="/" element={<Navigate to="/register" replace />} />
 
         <Route path="/register" element={<Register />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/admin/users" element={<PlaceholderPage title="Users" />} />
+        <Route path="/admin/subscriptions" element={<PlaceholderPage title="Subscriptions" />} />
+        <Route path="/admin/revenue" element={<PlaceholderPage title="Revenue" />} />
+        <Route path="/admin/product" element={<PlaceholderPage title="Product" />} />
+        <Route path="/admin/settings" element={<PlaceholderPage title="Settings" />} />
 
         {/* <Route path="/login" element={<LoginPage />} /> */}
 
